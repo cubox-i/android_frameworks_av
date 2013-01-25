29810000c43d9c2574b68281a5caab956b183d9d
b02247 <b02247@freescale.com>
2013-01-25 15:21:45 +0800

    ENGR00241758 Support large samplerate in audio framework
    Support large samplerate in audio framework.
    
    Signed-off-by: b02247 <b02247@freescale.com>

--- media/libmedia/AudioTrack.cpp
+++ media/libmedia/AudioTrack.cpp
@@ -2,7 +2,7 @@
 ** Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
 ** Not a Contribution.
 ** Copyright 2007, The Android Open Source Project
-** Copyright (C) 2012 Freescale Semiconductor, Inc.
+** Copyright (C) 2012-2013 Freescale Semiconductor, Inc.
 **
 ** Licensed under the Apache License, Version 2.0 (the "License");
 ** you may not use this file except in compliance with the License.
@@ -811,8 +811,8 @@ status_t AudioTrack::setSampleRate(uint32_t rate)
     if (AudioSystem::getOutputSamplingRate(&afSamplingRate, mStreamType) != NO_ERROR) {
         return NO_INIT;
     }
-    // Resampler implementation limits input sampling rate to 2 x output sampling rate.
-    if (rate == 0 || rate > afSamplingRate*2 ) {
+    // Resampler implementation limits input sampling rate to 8 x output sampling rate.
+    if (rate == 0 || rate > afSamplingRate*8 ) {
         return BAD_VALUE;
     }
 
--- services/audioflinger/Threads.cpp
+++ services/audioflinger/Threads.cpp
@@ -1345,8 +1345,8 @@ sp<AudioFlinger::PlaybackThread::Track> AudioFlinger::PlaybackThread::createTrac
                 lStatus = BAD_VALUE;
                 goto Exit;
         }
-        // Resampler implementation limits input sampling rate to 2 x output sampling rate.
-        if (sampleRate > mSampleRate*2) {
+        // Resampler implementation limits input sampling rate to 8 x output sampling rate.
+        if (sampleRate > mSampleRate*8) {
             ALOGE("Sample rate out of range: %u mSampleRate %u", sampleRate, mSampleRate);
             lStatus = BAD_VALUE;
             goto Exit;
